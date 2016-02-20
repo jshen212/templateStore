45edd7f4122ec4a3dc0163b4c45bd887be078003
simplejeff <jeffshen86@gmail.com>
2016-02-20 07:10:46 -0800

    [fix] working on layout of views

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -4,11 +4,12 @@ angular.module('templateStore.templates', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider){
   $routeProvider.
-    when('/templates', {
+    when('/templates',
+    {
       templateUrl: 'templates/templates.html',
       controller: 'TemplatesCtrl'
     });
 }])
 .controller('TemplatesCtrl', ['$scope', function($scope){
-  console.log($scope);
+  console.log('templates CONTROLLER');
 }]);
