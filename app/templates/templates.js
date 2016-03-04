4f0e42fe7694c258a90fc62d7d7271b5794e77fd
simplejeff <jeffshen86@gmail.com>
2016-03-03 22:46:12 -0800

    [feat] finished adding front end angular code to render template images in ng-repeat

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -19,10 +19,20 @@ angular.module('templateStore.templates', ['ngRoute'])
 
 .controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http){
   $http.get('json/templates.json').success(function(data){
-    console.log(data);
+    $scope.templates = data;
   });
 }])
 
-.controller('TemplateDetailsCtrl', ['$scope', function($scope){
+.controller('TemplateDetailsCtrl', ['$scope', '$routeParams', '$http', '$filter', function($scope, $routeParams, $http, $filter){
+  var templateId = $routeParams.templateId;
+  $http.get('json/templates.json').success(function(data){
+    $scope.template = $filter('filter')(data, function(d){
+      return d.id == templateId;
+    })[0];
+    $scope.mainImage = $scope.template.images[0].name;
+  });
 
+  $scope.setImage = function(image){
+      $scope.mainImage = image.name;
+  };
 }]);
