4d5ef9228e93b1442921b04a1f583200bf000e4c
simplejeff <jeffshen86@gmail.com>
2016-03-03 22:15:03 -0800

    [feat] added routes

--- app/templates/templates.js
+++ app/templates/templates.js
@@ -4,12 +4,25 @@ angular.module('templateStore.templates', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider){
   $routeProvider.
-    when('/templates',
-    {
-      templateUrl: 'templates/templates.html',
-      controller: 'TemplatesCtrl'
-    });
+  when('/templates',
+  {
+    templateUrl: 'templates/templates.html',
+    controller: 'TemplatesCtrl'
+  }).
+
+  when('/templates/:templateId',
+  {
+    templateUrl: 'templates/template-details.html',
+    controller: 'TemplateDetailsCtrl'
+  });
 }])
-.controller('TemplatesCtrl', ['$scope', function($scope){
-  console.log('templates CONTROLLER');
+
+.controller('TemplatesCtrl', ['$scope', '$http', function($scope, $http){
+  $http.get('json/templates.json').success(function(data){
+    console.log(data);
+  });
+}])
+
+.controller('TemplateDetailsCtrl', ['$scope', function($scope){
+
 }]);
